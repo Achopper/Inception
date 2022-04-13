6025d9b8a93262c384d76b1bdd8ff90482fce33b
achopper <achopper@student.21-school.ru>
2022-04-13 21:05:49 +0300

    add containers & yaml

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+up:
+	docker-compose -f src/docker-compose.yaml buid
+	docker-compose -f src/docker-compose.yaml up -d
+
+down:
+	docker-compose -f src/docker-compose.yaml down
+
+re:
+	docker-compose -f src/docker-compose.yaml up --build
+
---
+++ src/wp/start.sh
@@ -0,0 +1,14 @@
+
+
+
+chmod -R 775 /var/www/html/wordpress;
+chown -R www-data /var/www/html/wordpress;
+chgrp -R www-data /var/www/html/wordpress;
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+php wp-cli.phar --info;
+chmod +x wp-cli.phar;
+sudo mv wp-cli.phar /usr/local/bin/wp;
+wp core install --allow-root --url=achopper.42.fr --title=21 --admin_user=achopper --admin_email=achopper@boss.com;
+wp user create dimon dimon@di.com --role=author --allow-root;
