d3d86591261e57d900b96542e64dce1eb409617d
achopper <achopper@student.21-school.ru>
2022-04-14 21:47:51 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 sed -i "s/bind-address/\#bind-address/" "/etc/mysql/mariadb.conf/50-server.cnf"
 sed -i "s/\#port/port/" "/etc/mysql/mariadb.conf/50-server.cnf"
-chown -R mysql:mysqld /var/lib/mysql
+chown -R mysqld:mysqld /var/lib/mysql
 if [ ! -d /var/lib/mysql/wp ]; then
-  service mysql start
+  service mysqld start
   mysql -u root -e "CREATE DATABASE IF NOT EXIST $DB_NAME"
   mysql -u root -e "CREATE USER IF NOT EXIST '$DP_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION"
   mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DP_USER'@'%'"
   mysql -u root -e "FLUSH PRIVILEGES"
-  musqladmin -u root password $DB_ROOT_PASSWORD
-  service mysql stop
+  mysqladmin -u root password $DB_ROOT_PASSWORD
+  service mysqld stop
 else
   mkdir /var/run/mysqld
   touch /var/run/mysqld/mysqld.pid
   mkfifo /var/run/mysqld/mysqld.sock
 fi
-chown -R mysql var/run/mysqld
+chown -R mysql /var/run/mysqld
 exec "$@"
