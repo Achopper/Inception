6064c82d65aa7d8a3a3c7b195a229e685d2f3361
achopper <achopper@student.21-school.ru>
2022-04-14 21:03:16 +0300

    fix

--- src/wp/start.sh
+++ src/wp/start.sh
@@ -2,8 +2,8 @@
 
 
 chmod -R 775 /var/www/html/wordpress;
-chown -R www-data /var/www/html/wordpress;
-chgrp -R www-data /var/www/html/wordpress;
+chown -R www /var/www/html/wordpress;
+chgrp -R www /var/www/html/wordpress;
 
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
@@ -12,3 +12,5 @@ chmod +x wp-cli.phar;
 sudo mv wp-cli.phar /usr/local/bin/wp;
 wp core install --allow-root --url=achopper.42.fr --title=21 --admin_user=achopper --admin_email=achopper@boss.com;
 wp user create dimon dimon@di.com --role=author --allow-root;
+
+/usr/sbin/php-fpm7.3 -F
