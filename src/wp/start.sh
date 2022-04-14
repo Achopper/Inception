bcb2fd10ca8883f65c3ff810361f98ac04a95627
achopper <achopper@student.21-school.ru>
2022-04-14 22:23:55 +0300

    fix

--- src/wp/start.sh
+++ src/wp/start.sh
@@ -10,7 +10,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 php wp-cli.phar --info;
 chmod +x wp-cli.phar;
 sudo mv wp-cli.phar /usr/local/bin/wp;
-wp core install --allow-root --url=achopper.42.fr --title=21 --admin_user=achopper --admin_email=achopper@boss.com;
+wp core download --allow-root --url=achopper.42.fr --title=21 --admin_user=achopper --admin_email=achopper@boss.com;
 wp user create dimon dimon@di.com --role=author --allow-root;
 
 /usr/sbin/php-fpm7.3 -F
