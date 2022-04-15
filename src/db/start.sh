bbd3f08d6e96a82f5e38f5933b7d0bafaf758ac4
achopper <achopper@student.21-school.ru>
2022-04-15 19:30:25 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -5,16 +5,16 @@
 chown -R mysql:mysql /var/lib/mysql
 if [ ! -d /var/lib/mysql/wordpress ]; then
   service mysql start
-  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-  mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+  mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
   mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
-  mysql -u root -e "FLUSH PRIVILEGES"
+  mysql -u root -e "FLUSH PRIVILEGES;"
   mysqladmin -u root password $DB_ROOT_PASSWORD
 service mysql stop
 #else
-  mkdir /var/run/mysqld
-  touch /var/run/mysqld/mysqld.pid
-  mkfifo /var/run/mysqld/mysqld.sock
+#  mkdir /var/run/mysqld
+#  touch /var/run/mysqld/mysqld.pid
+#  mkfifo /var/run/mysqld/mysqld.sock
 fi
 #chown -R mysql /var/run/mysqld
 #exec "$@"
