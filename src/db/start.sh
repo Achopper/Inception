637e516662248deaf250e3a598b396226de09b8e
achopper <achopper@student.21-school.ru>
2022-04-15 20:01:25 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env sh
-#sed -i "s/bind-address/\#bind-address/" /etc/mysql/mariadb.conf.d/50-server.cnf
-#sed -i "s/\#port/port/" "/etc/mysql/mariadb.conf.d/50-server.cnf"
-
 chown -R mysql:mysql /var/lib/mysql
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-  service mysql start
-  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-  mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-  mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
-  mysql -u root -e "FLUSH PRIVILEGES;"
-  mysqladmin -u root password $DB_ROOT_PASSWORD
+
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+service mysql start
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
+mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysqladmin -u root password $DB_ROOT_PASSWORD
 service mysql stop
-#else
-#  mkdir /var/run/mysqld
-#  touch /var/run/mysqld/mysqld.pid
-#  mkfifo /var/run/mysqld/mysqld.sock
 fi
-#chown -R mysql /var/run/mysqld
-#exec "$@"
+
 /usr/bin/mysqld_safe
