4b63f53df885b976b9ba14b04a2c6e98597a7dfb
achopper <achopper@student.21-school.ru>
2022-04-15 22:02:28 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -3,13 +3,13 @@
 chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/wp" ]; then
-service mariadb start
+service mysql start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wp DEFAULT CHARACTER SET utf8;"
 mysql -u root -e "CREATE USER 'achopper'@'%' IDENTIFIED BY '123';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wp.* TO achopper@'%'"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -u root password 123
-service mariadb stop
+service mysql stop
 fi
 
 /usr/bin/mysqld_safe
