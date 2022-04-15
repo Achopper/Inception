2c37933a57514539829d32a80b4f14098ecdc427
achopper <achopper@student.21-school.ru>
2022-04-15 19:18:04 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -6,7 +6,7 @@ chown -R mysql:mysql /var/lib/mysql
 if [ ! -d /var/lib/mysql/wordpress ]; then
   service mysql start
   mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress"
-  mysql -u root -e "CREATE USER IF NOT EXISTS 'achopper'@'%' IDENTIFIED BY '1234' WITH GRANT OPTION"
+  mysql -u root -e "CREATE USER IF NOT EXISTS 'achopper'@'%' IDENTIFIED BY '1234'"
   mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'achopper'@'%'"
   mysql -u root -e "FLUSH PRIVILEGES"
   mysqladmin -u root password "1234"
