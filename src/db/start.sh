9d248049160020044ca8ce522464da5e0490b5d8
achopper <achopper@student.21-school.ru>
2022-04-15 19:23:06 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -5,12 +5,11 @@
 chown -R mysql:mysql /var/lib/mysql
 if [ ! -d /var/lib/mysql/wordpress ]; then
   service mysql start
-  mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress"
-  mysql -u root -e "CREATE USER IF NOT EXISTS 'achopper'@'%' IDENTIFIED BY '1234'"
-  mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'achopper'@'%'"
+  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+  mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+  mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
   mysql -u root -e "FLUSH PRIVILEGES"
-  mysqladmin -u root password "1234"
-#  $DB_ROOT_PASSWORD
+  mysqladmin -u root password $DB_ROOT_PASSWORD
 service mysql stop
 #else
 #  mkdir /var/run/mysqld
