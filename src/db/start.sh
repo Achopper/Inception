91adf960f81148d39d2e9ceba0a2b77ea46cf4b8
achopper <achopper@student.21-school.ru>
2022-04-15 19:04:53 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -2,8 +2,8 @@
 #sed -i "s/bind-address/\#bind-address/" /etc/mysql/mariadb.conf.d/50-server.cnf
 #sed -i "s/\#port/port/" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 
-chown -R mysqld:mysqld /var/lib/mysql
-if [ ! -d /var/lib/mysql/wp ]; then
+chown -R mysql:mysql /var/lib/mysql
+if [ ! -d /var/lib/mysql/wordpress ]; then
   service mysqld start
   mysql -u root -e "CREATE DATABASE IF NOT EXIST $DB_NAME"
   mysql -u root -e "CREATE USER IF NOT EXIST '$DP_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION"
@@ -11,10 +11,11 @@ if [ ! -d /var/lib/mysql/wp ]; then
   mysql -u root -e "FLUSH PRIVILEGES"
   mysqladmin -u root password $DB_ROOT_PASSWORD
 service mysqld stop
-else
-  mkdir /var/run/mysqld
-  touch /var/run/mysqld/mysqld.pid
-  mkfifo /var/run/mysqld/mysqld.sock
+#else
+#  mkdir /var/run/mysqld
+#  touch /var/run/mysqld/mysqld.pid
+#  mkfifo /var/run/mysqld/mysqld.sock
 fi
-chown -R mysql /var/run/mysqld
-exec "$@"
+#chown -R mysql /var/run/mysqld
+#exec "$@"
+mysqld_safe
