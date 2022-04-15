9f4dcbaeb0809268762eb05447b5706d8d20f70b
achopper <achopper@student.21-school.ru>
2022-04-15 20:22:11 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -2,13 +2,13 @@
 chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-service mysqld start
+service mysql start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
 mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -u root password $DB_ROOT_PASSWORD
-service mysqld stop
+service mysql stop
 fi
 
 /usr/bin/mysqld_safe
