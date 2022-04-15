312bd890a38c1c9e9265677a2dae011d7c8b2397
achopper <achopper@student.21-school.ru>
2022-04-15 19:33:17 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -7,7 +7,7 @@ if [ ! -d /var/lib/mysql/wordpress ]; then
   service mysql start
   mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
   mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-  mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
+  mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
   mysql -u root -e "FLUSH PRIVILEGES;"
   mysqladmin -u root password $DB_ROOT_PASSWORD
 service mysql stop
