1c6525da17f9c33af29612fd1150dfec874bfc53
achopper <achopper@student.21-school.ru>
2022-04-15 20:47:15 +0300

    fix

--- src/db/start.sh
+++ src/db/start.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 chown -R mysql:mysql /var/lib/mysql
 
-#if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 service mysql start
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME DEFAULT CHARACTER SET utf8;"
-mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wp DEFAULT CHARACTER SET utf8;"
+mysql -u root -e "CREATE USER 'achopper'@'%' IDENTIFIED BY '123';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wp.* TO achopper@'%'"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysqladmin -u root password $DB_ROOT_PASSWORD
+mysqladmin -u root password 123
 service mysql stop
-#fi
+fi
 
 /usr/bin/mysqld_safe
